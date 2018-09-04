c8a9cf7044eb6be901de54513ef9354439f0c814
Vytautas Sipavicius <vytasip@hotmail.com>
2018-09-04 14:56:03 +0000

    Fixed up so numbers are printed in seperate lines

--- Grunnur/daematimaverkefni_git_algorithm/sequence.py
+++ Grunnur/daematimaverkefni_git_algorithm/sequence.py
@@ -7,11 +7,12 @@ n = int(input("Enter the length of the sequence: ")) # Do not change this line
 first_num = 1
 second_num = 2
 third_num = 3
-print("1,2,3", end="")
+print("1")
+print("2")
+print("3")
 for x in range(n-3):
     sum = first_num + second_num + third_num
-    print("," + str(sum), end="")
+    print(sum)
     first_num = second_num
     second_num = third_num
     third_num = sum
-print("")
