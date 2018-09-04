0f2360c94a336ab57e2b1c90de8dd9456879d61d
Vytautas Sipavicius <vytasip@hotmail.com>
2018-09-04 13:55:31 +0000

    Algorithm written for max_int

---
+++ Grunnur/daematimaverkefni_git_algorithm/max_int.py
@@ -0,0 +1,5 @@
+# Algorithm for finding the largest number from different numbers
+# 1. Compare the first number to the second one. the larger number is now the largest number
+# 2. If there is another number to comparek, take the largest number and compare it to the next number, the larger number is now the largest number.
+# 3. Repeat step 2 until there are no more numbers left to compare
+# 4. Your largest number will be the larger number of the last comparison
