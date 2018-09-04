ee49334d41dce52353d367d6d0561582900e7693
Vytautas Sipavicius <vytasip@hotmail.com>
2018-09-04 14:22:23 +0000

    Take in mupltiple inputs from user

--- Grunnur/daematimaverkefni_git_algorithm/max_int.py
+++ Grunnur/daematimaverkefni_git_algorithm/max_int.py
@@ -10,4 +10,5 @@ max_int = num_int
 while num_int > 0:
     if num_int > max_int:
         max_int = num_int
+    num_int = int(input("Input a number: "))
 print("The maximum is", max_int)    # Do not change this line
