0fcf02d1ebb7233b5a9405677df73e22461cb08d
Vytautas Sipavicius <vytasip@hotmail.com>
2018-09-04 14:43:34 +0000

    Added a step to the algorithm and implemented code

--- Grunnur/daematimaverkefni_git_algorithm/sequence.py
+++ Grunnur/daematimaverkefni_git_algorithm/sequence.py
@@ -1,4 +1,18 @@
 #Algorithm for a sequence
 # 1. Initialize with 1, 2 and 3 as the first three numbers
 # 2. To find the next number add the last three together
-# 3. Repeat step 2 for n-3 times
+# 3. shift the 3 numbers down one place and make the sum of them the 3rd number.  
+# 4. Repeat step 2 for n-3 times
+n = int(input("Enter the length of the sequence: ")) # Do not change this line
+first_num = 1
+second_num = 2
+third_num = 3
+print("1,2,3" + end='')
+print(first_num, second_num, third_num)
+for x in range(n-3):
+    sum = first_num + second_num + third_num
+    print(sum + end='')
+    first_num = second_num
+    second_num = third_num
+    third_num = sum
+print("")
