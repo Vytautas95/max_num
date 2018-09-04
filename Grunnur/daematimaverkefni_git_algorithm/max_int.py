a0de9529ca4021c2845aa57a8cf5a2fa15959628
Vytautas Sipavicius <vytasip@hotmail.com>
2018-09-04 14:06:38 +0000

    Code implemented

--- Grunnur/daematimaverkefni_git_algorithm/max_int.py
+++ Grunnur/daematimaverkefni_git_algorithm/max_int.py
@@ -3,3 +3,11 @@
 # 2. If there is another number to comparek, take the largest number and compare it to the next number, the larger number is now the largest number.
 # 3. Repeat step 2 until there are no more numbers left to compare
 # 4. Your largest number will be the larger number of the last comparison
+
+#Code implementation for finding the largest number
+num_int = int(input("Input a number: "))    # Do not change this line
+max_int = num_int
+while num_int > 0:
+    if num_int > max_int:
+        max_int = num_int
+print("The maximum is", max_int)    # Do not change this line
